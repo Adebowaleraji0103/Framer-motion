f8c140d8a3c01bd1060b19bf6a615c279ef7f873
Mubarak <adebowaleraji0103@gmail.com>
2025-07-03 21:33:22 +0100

    s

--- src/component/Rocket.jsx
+++ src/component/Rocket.jsx
@@ -3,14 +3,22 @@ import { motion } from "framer-motion";
 
 const Rocket = () => {
   return (
-    <div
-      className="flex justify-center flex-col text-white mt-9 text-center font-bold text-5xl "
-      id="rocket"
-    >
-      <motion.h1 animate={{ color: "red" }} className="mb-[3rem]">
-        Rocket
-      </motion.h1>
-      <RocketImage />
+    <div>
+      <div
+        className="flex justify-center flex-col text-white mt-9 text-center font-bold text-5xl "
+        id="rocket"
+      >
+        <motion.h1 animate={{ color: "red" }} className="mb-[3rem]">
+          Rocket
+        </motion.h1>
+        <RocketImage />
+      </div>
+
+      <div className="flex justify-center">
+        <motion.button className="bg-white rounded-[15px] font-bold w-fit text-black px-6 py-2 text-[20px]">
+          Hello
+        </motion.button>
+      </div>
     </div>
   );
 };
