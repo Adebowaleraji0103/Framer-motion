e7705c8a71986f0c6c52152d91c8fed54ce6b0be
Mubarak <adebowaleraji0103@gmail.com>
2025-07-06 23:51:51 +0100

    ke

--- src/component/Rocket.jsx
+++ src/component/Rocket.jsx
@@ -28,7 +28,7 @@ const Rocket = () => {
         whileHover={{
           scale:1.2,
           boxShadow:"3px 3px 3px lightblue",
-          color:""
+          color:"yellow"
         }}
 
         >Welcome</motion.button>
