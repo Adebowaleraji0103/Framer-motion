86915a4ac2fb83d873bf8b2d87da329d387bb344
Mubarak <adebowaleraji0103@gmail.com>
2025-07-08 02:38:46 +0100

    s

--- src/component/Rocket.jsx
+++ src/component/Rocket.jsx
@@ -2,12 +2,13 @@ import RocketImage from "./RocketImage";
 import { hover, motion } from "framer-motion";
 
 const Rocket = () => {
-
-  const buttonVariant ={
+  const buttonVariant = {
     hover: {
-      
-    }
-  }
+      scale: 1.2,
+      boxShadow: "3px 3px 3px lightblue",
+      color: "yellow",
+    },
+  };
   return (
     <div>
       <div
@@ -21,23 +22,28 @@ const Rocket = () => {
       </div>
 
       <div className="flex justify-center">
-        <motion.button className="bg-white rounded-[15px] font-bold w-fit text-black px-6 py-2 text-[20px]"
-          initial={{ x: 10, opacity:0 }}
-          animate={{ color: "red", x: 0, opacity:1}}
-          transition={{ delay:3,duration: 5, type:"spring", stiffness:1200 }}
+        <motion.button
+          className="bg-white rounded-[15px] font-bold w-fit text-black px-6 py-2 text-[20px]"
+          initial={{ x: 10, opacity: 0 }}
+          animate={{ color: "red", x: 0, opacity: 1 }}
+          transition={{
+            delay: 3,
+            duration: 5,
+            type: "spring",
+            stiffness: 1200,
+          }}
         >
           Hello
         </motion.button>
       </div>
       <div className="flex justify-center mt-8 mb-20">
-        <motion.button className=" text-white bg-[blue] px-5 py-2 font-bold w-fit text-[20px] rounded-[10px]"
-        whileHover={{
-          scale:1.2,
-          boxShadow:"3px 3px 3px lightblue",
-          color:"yellow"
-        }}
-
-        >Welcome</motion.button>
+        <motion.button
+          className=" text-white bg-[blue] px-5 py-2 font-bold w-fit text-[20px] rounded-[10px]"
+          variants={buttonVariant}
+          whileHover= "hover"
+        >
+          Welcome
+        </motion.button>
       </div>
     </div>
   );
